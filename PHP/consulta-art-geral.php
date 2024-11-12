46481ebe131b1430dc3ad8c145eba46ae3d40117
Italo <85656822+italopaladino@users.noreply.github.com>
2024-11-11 23:41:58 -0300

    att formulário

--- PHP/consulta-art-geral.php
+++ PHP/consulta-art-geral.php
@@ -24,10 +24,10 @@ try {
     
     // Cria a frase com os dados do autor, título sublinhado, DOI e data
     $frase = "<div class='citation' >
-                <a class='link-pesq' href='../HTML/resultados.php?id=" . htmlspecialchars($infogera['geralid']) . "'>". htmlspecialchars($infogera['correspondente'])
+                <a class='link-pesq' href='../HTML/resultados.php?id=" . htmlspecialchars($infogera['geralid']) . "'>". "<b>Correspondente:</b> ".htmlspecialchars($infogera['correspondente'])
                  . "(" . htmlspecialchars($infogera['email']) . ")"
-                 . "</br>" ."Titulo Projeto principal associado: " . htmlspecialchars($infogera['tituloprinc']) . 
-                "</br>".  htmlspecialchars($infogera['titulodado']) . "</a>
+                 . "</br>" ."<b>Título  do projeto principal:</b> " . htmlspecialchars($infogera['tituloprinc']) . 
+                "</br>". "<b>Título do dado:</b> " . htmlspecialchars($infogera['titulodado']) . "</a>
               </div></br></br>";
 
     // Exibe a frase dentro de uma célula da tabela
